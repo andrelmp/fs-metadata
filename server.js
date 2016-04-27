bf961306fdcb750bcd8af06734a239b05385af05
Andre Pasos <andre-ap2@hotmail.com>
2016-04-27 17:20:13 +0000

    challenge solution

---
+++ server.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var express = require('express');
+var multer = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage});
+
+var app = express();
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
+
+app.get('/', function(req, res) {
+    res.sendFile(process.cwd() + '/public/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('0'), function(req, res) {
+    res.json({ fileSize: req.file.size });
+});
+
+
+app.listen(8080, function() {
+    console.log('Node.js listening on port 8080...');
+});
